aa583cfdfc26d7ebcf1acce9e97ff02e6c11a49e
avrahamm <papaabramg@gmail.com>
2019-09-15 14:32:38 +0300

    server - added cache

--- src/Repository/YearRepository.php
+++ src/Repository/YearRepository.php
@@ -2,9 +2,12 @@
 
 namespace App\Repository;
 
-use App\Entity\Year;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Contracts\Cache\ItemInterface;
 use Doctrine\Common\Persistence\ManagerRegistry;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+
+use App\Entity\Year;
 
 /**
  * @method Year|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,25 +17,49 @@ use Doctrine\Common\Persistence\ManagerRegistry;
  */
 class YearRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
+    public function __construct(ManagerRegistry $registry, TagAwareCacheInterface $cache)
     {
         parent::__construct($registry, Year::class);
+        $this->cache = $cache;
+    }
+
+    /**
+     * Returns the year data.
+     *
+     * @param $year
+     * @return mixed
+     * @throws \Psr\Cache\InvalidArgumentException
+     */
+    public function getYearData($year)
+    {
+        $yearData = $this->cache->get("yearData{$year}", function (ItemInterface $item) use ($year) {
+            $item->expiresAfter(3600);
+            return $this->findOneBy(['year'=>$year]);
+        });
+        return $yearData;
     }
 
     /**
      * Returns all years.
-     * @return array of Objects like {"year": "1983"}
      *
+     * @return array of Objects like {"year": "1983"}
+     * @throws \Psr\Cache\InvalidArgumentException never happens
      */
     public function findAllYears() : array
     {
-        $temp = 1;
-        return $this->createQueryBuilder('y')
-            ->select('y.year')
-            ->orderBy('y.year', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-            ;
+        $allYears = $this->cache->get('allYears', function (ItemInterface $item) {
+            $item->expiresAfter(3600);
+            return $this->createQueryBuilder('y')
+                ->select('y.year')
+                ->orderBy('y.year', 'ASC')
+                ->setMaxResults(10)
+                ->getQuery()
+                ->getResult()
+                ;
+        });
+        return $allYears;
     }
+
+    /** @var TagAwareCacheInterface */
+    private $cache;
 }
