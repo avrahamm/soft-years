8e7948d9add0985c4329646240cd33e70c8059aa
avrahamm <papaabramg@gmail.com>
2019-09-18 13:12:13 +0300

    react1 - encore and twig

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,6 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
